b7e5f2dec0279c6065df9aee04c071b4e3b62734
Surbhi Inani <surbhiinani@yahoo.com>
2016-09-17 03:38:46 -0400

    Update content.js

--- HackCMU2/content.js
+++ HackCMU2/content.js
@@ -1,1 +1,17 @@
-alert("Hello World")
+$(document).ready(function()
+	{
+	//set the font size
+	var x = $('p,div,span,h1,h2,h3,h4,h5,h6');
+	
+	for (c in x)
+	{
+	var currSize = parseInt($(x[c]).css('font-size'));
+	
+	console.log(currSize);
+	if (currSize < 15)
+		$(x[c]).css({'font-size' : 15});
+	else
+		$(x[c]).css({'font-size' : currSize+currSize});
+	}
+	});
+	
