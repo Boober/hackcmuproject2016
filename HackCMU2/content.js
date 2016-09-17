57dbcf91c09b90ce47886d245bbabded467911c3
Eli Yu <yilunyu@gmail.com>
2016-09-17 05:19:48 -0400

    functional text to speech

--- HackCMU2/content.js
+++ HackCMU2/content.js
@@ -1,1 +1,12 @@
-alert("Hello World")
+//alert("Hello World");
+var synth = window.speechSynthesis;
+
+$("p").on("click", function(){
+  var utterThis = new SpeechSynthesisUtterance($(this).text());
+  synth.speak(utterThis);
+});
+
+window.onunload = function(){
+  console.log("hello");
+  synth.cancel();
+};
